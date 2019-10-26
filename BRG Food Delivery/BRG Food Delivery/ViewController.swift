64cdfa476c745e9620bc2d44f8a553ec1e9afd26
Zubair Hasan <zubo@octoriz.com>
2019-10-27 02:11:21 +0600

    Appicon added

---
+++ BRG Food Delivery/BRG Food Delivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BRG Food Delivery
+//
+//  Created by Zubair Hasan on 27/10/19.
+//  Copyright © 2019 Octoriz Private Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
